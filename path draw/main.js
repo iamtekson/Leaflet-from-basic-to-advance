99043871a37e1ad6b3e717f394d114167e7254de
iamtekson <teksondada@gmail.com>
2021-01-31 01:20:07 +0700

    leaflet path draw function

--- path draw/main.js
+++ path draw/main.js
@@ -4,31 +4,13 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// to get the height of the window
-var height = $(window).height();
-console.log(`the height of the pc is: ` + height);
-
-
-    // var point = map.on('click', function(e){
-    //     return(new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map));
-    // });
-
 var marker = null;
 var line = L.polyline([]).addTo(map);
-    function draw(){
-        map.on('click', function(e){
-            var point = {lat: e.latlng.lat, lng: e.latlng.lng};
-            if(!marker) {
-                marker = L.marker(point).addTo(map);
-            }
-        line.addLatLng(point);
-        });
-            
-    }
-draw();
 
-// map.on('click', function(e){
-//     var point = {lat: e.latlng.lat, lng: e.latlng.lng},
-//         marker = L.marker([e.latlng.lat, e.latlng.lng]);
-//     line.addLatLng(point);
-// });
+map.on('click', function(e){
+    var point = {lat: e.latlng.lat, lng: e.latlng.lng};
+    if(!marker) {
+        marker = L.marker(point).addTo(map);
+    }
+line.addLatLng(point);
+});
